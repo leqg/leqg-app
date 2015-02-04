bf770a403f4a18bd78ffe5afc7ca05c623ced54f
Pierre Rudloff <contact@rudloff.pro>
2015-02-04 23:28:32 +0100

    Keep last used email in localStorage

--- www/js/auth.js
+++ www/js/auth.js
@@ -12,15 +12,21 @@ var auth = (function () {
         );
     }
     function login() {
+        var email = $('#auth_email').val();
+        localStorage.setItem('auth_email', email);
         $.ajax(
             {
                 url: BASE_URL + 'authenticate',
                 success: getToken,
-                headers: { 'Authorization': 'Basic ' + window.btoa($('#auth_email').val() + ':' + $('#auth_pass').val()) }
+                headers: { 'Authorization': 'Basic ' + window.btoa(email + ':' + $('#auth_pass').val()) }
             }
         );
     }
     my.init = function () {
+        var email = localStorage.getItem('auth_email');
+        if (email) {
+            $('#auth_email').val(email);
+        }
         $('#auth_login_btn').click(login);
     };
     return my;
