306e9632c09291dfaf73c87f94767f80cdd89408
Pierre Rudloff <contact@rudloff.pro>
2015-02-06 00:49:00 +0100

    Submit forms with enter

--- www/js/common.js
+++ www/js/common.js
@@ -9,7 +9,13 @@ var common = (function () {
     my.init = function () {
         $window.on('offline', lostConnection);
     };
+    my.submitForm = function (e) {
+        if (e.keyCode === 13) {
+            $(':mobile-pagecontainer').pagecontainer('getActivePage').find('.submit').click();
+        }
+    };
     return my;
 }());
 
 $(document).ready(common.init);
+$(window).keypress(common.submitForm);
